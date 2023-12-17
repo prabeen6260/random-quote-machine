38cce20c538dff09dd5e0c4977842caf5717bcaf
Prabeen <prabingiri922@gmail.com>
2023-12-17 08:31:11 -0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,64 @@
 import logo from './logo.svg';
 import './App.css';
+import  q from './dataset.js'
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const box = {
+  height: '${quoteToDisplay*15}px',
+  width: '300px',
+  borderRadius: '2px',
+  padding:'30px',
+  backgroundColor: 'white',
+  display: 'flex',
+  flexDirection: 'column',
+  justifyContent: 'center',  // Horizontal centering
+  alignItems: 'center',
+  fontSize:'23px',
+  Font:''      // Vertical centering
+};
+
+const body = {
+  backgroundColor: 'black',
+  height: '100vh',  // Full viewport height
+  display: 'flex',
+  justifyContent: 'center',  // Horizontal centering for body
+  alignItems: 'center',      // Vertical centering for body
+};
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state={
+      index: ''
+    }
+    this.handleClick=this.handleClick.bind(this);
+  }
+handleClick(){
+  this.setState({index: Math.floor(Math.random()*q.quote.length)});
+}
+
+  render() {
+    let quoteToDisplay=q.quote[this.state.index];
+    let authorToDisplay=q.author[this.state.index]
+    return (
+      <div style={body}>
+        <div style={box}>
+          {this.state.index === '' && (
+            <>
+              {q.quote[0]} <br /> -{q.author[0]}
+            </>
+          )}
+          {this.state.index !== '' && (
+            <>
+              {quoteToDisplay} <br /> -{authorToDisplay}
+            </>
+          )}
+          <button onClick={this.handleClick}>New Quote</button>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
+
